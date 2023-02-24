57a86f03b104acd8666e900d5cf0a76fb77ff4fd
jmorenog1986 <jhonmorenogarcia@gmail.com>
2023-02-23 21:26:06 -0500

    Se sube proyecto al repositorio

---
+++ ClaseCSharp/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace ClaseCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.tablaPersonas = new System.Windows.Forms.DataGridView();
+            this.btnConsultarTodos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaPersonas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Documento";
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(173, 58);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(282, 20);
+            this.txtDocumento.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(174, 98);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(282, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(173, 139);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(282, 20);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Apellido";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(173, 176);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(282, 20);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Email";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(175, 234);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 8;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(256, 234);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 9;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(337, 234);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(418, 234);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 11;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // tablaPersonas
+            // 
+            this.tablaPersonas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaPersonas.Location = new System.Drawing.Point(647, 32);
+            this.tablaPersonas.Name = "tablaPersonas";
+            this.tablaPersonas.Size = new System.Drawing.Size(663, 325);
+            this.tablaPersonas.TabIndex = 12;
+            // 
+            // btnConsultarTodos
+            // 
+            this.btnConsultarTodos.Location = new System.Drawing.Point(647, 363);
+            this.btnConsultarTodos.Name = "btnConsultarTodos";
+            this.btnConsultarTodos.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultarTodos.TabIndex = 13;
+            this.btnConsultarTodos.Text = "Consultar Todos";
+            this.btnConsultarTodos.UseVisualStyleBackColor = true;
+            this.btnConsultarTodos.Click += new System.EventHandler(this.btnConsultarTodos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1496, 450);
+            this.Controls.Add(this.btnConsultarTodos);
+            this.Controls.Add(this.tablaPersonas);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tablaPersonas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.DataGridView tablaPersonas;
+        private System.Windows.Forms.Button btnConsultarTodos;
+    }
+}
+
---
+++ ClaseCSharp/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ClaseCSharp
+{
+  
+    public partial class Form1 : Form
+       
+    {
+        int documento;
+        String nombre;
+        String apellido;
+        String email;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            llenar();
+            String query = "INSERT INTO Personas VALUES('"+documento+"','"+nombre+"','"+apellido+"','"+email+"')";
+            ejecutar(query,1);
+           
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            llenar();
+            String query = "UPDATE Personas SET nombre='" + nombre + "', apellido='" + apellido + "', email='" + email + "' WHERE documento='" + documento + "'";
+            ejecutar(query,1);
+        }
+
+        public void llenar()
+        {
+             documento = Int16.Parse(txtDocumento.Text);
+             nombre = txtNombre.Text;
+             apellido = txtApellido.Text;
+             email = txtEmail.Text;
+        }
+
+        private void ejecutar(String query, int op)
+        {
+            SqlConnection conexion = new SqlConnection("Data Source=192.168.1.24;Initial Catalog=db01;Persist Security Info=True;User ID=SQLServer01;Password=123456");
+            conexion.Open();
+           
+
+            SqlCommand comando = new SqlCommand(query, conexion);
+            if (op==1)
+            {
+                comando.ExecuteNonQuery();
+            }else if (op==2)
+            {
+                SqlDataReader r=comando.ExecuteReader();
+                while (r.Read())
+                {
+                    txtDocumento.Text = r["documento"].ToString();
+                    txtNombre.Text= r["nombre"].ToString();
+                    txtApellido.Text= r["apellido"].ToString();
+                    txtEmail.Text=r["email"].ToString();
+                }
+            }
+           
+
+            conexion.Close();
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            llenar();
+            String query = "DELETE FROM personas WHERE documento='"+documento+"'";
+            ejecutar(query,1);
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            llenar();
+
+            String query = "SELECT * FROM personas WHERE documento='"+documento+"'";
+            ejecutar(query, 2);
+            
+        }
+
+        private void btnConsultarTodos_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable("Personas");
+            dt.Columns.Add("Documento", typeof(int));
+            dt.Columns.Add("Nombre", typeof(string));
+            dt.Columns.Add("Apellido", typeof(string));
+            dt.Columns.Add("Email", typeof(string));
+            // Agregar algunos datos
+
+
+            SqlConnection conexion = new SqlConnection("Data Source=192.168.1.24;Initial Catalog=db01;Persist Security Info=True;User ID=SQLServer01;Password=123456");
+            conexion.Open();
+            String query = "SELECT * FROM personas";
+
+            SqlCommand comando = new SqlCommand(query, conexion);
+            SqlDataReader r = comando.ExecuteReader();
+            while (r.Read())
+            {
+
+                dt.Rows.Add(
+                        r["documento"].ToString(),
+                        r["nombre"].ToString(),
+                        r["apellido"].ToString(),
+                        r["email"].ToString()
+                    );
+
+            }
+
+            
+            // Asignar el DataTable como origen de datos del DataGridView
+            tablaPersonas.DataSource = dt;
+
+
+            conexion.Close();
+        }
+    }
+}
